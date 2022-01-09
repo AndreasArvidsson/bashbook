efb866c264fdc328cb75e654b20c05c9abce8d6e
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-01-09 11:01:07 +0100

    Resize terminal to fit content

--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -3,12 +3,22 @@ import { Terminal } from "xterm";
 import "xterm/css/xterm.css";
 import "./renderer.css";
 
-export const activate: ActivationFunction = (context) => {
-  const map = new Map<string, Terminal>();
+interface TerminalState {
+  term: Terminal;
+  content: string;
+}
 
-  const createTerminal = (uri: string, element: HTMLElement) => {
+const COLS = 80;
+const ROWS_MAX = 30;
+
+export const activate: ActivationFunction = (context) => {
+  const map = new Map<string, TerminalState>();
+
+  const createTerminal = (uri: string) => {
     const term = new Terminal({
       rendererType: "dom",
+      cols: COLS,
+      rows: 1,
     });
 
     if (context.postMessage) {
@@ -20,8 +30,6 @@ export const activate: ActivationFunction = (context) => {
       });
     }
 
-    term.open(element);
-
     return term;
   };
 
@@ -29,11 +37,14 @@ export const activate: ActivationFunction = (context) => {
     const has = map.has(uri);
     if (create || !has) {
       if (has) {
-        const term = map.get(uri)!;
-        term.clear();
-        term.open(element);
+        const state = map.get(uri)!;
+        state.content = "";
+        state.term.clear();
+        state.term.open(element);
       } else {
-        map.set(uri, createTerminal(uri, element));
+        const term = createTerminal(uri);
+        term.open(element);
+        map.set(uri, { term, content: "" });
       }
     }
     return map.get(uri)!;
@@ -42,8 +53,14 @@ export const activate: ActivationFunction = (context) => {
   return {
     renderOutputItem(outputItem, element) {
       const { uri, data, create } = outputItem.json();
-      const term = getTerminal(uri, create, element);
-      term.write(data);
+      const state = getTerminal(uri, create, element);
+      state.term.write(data);
+      state.content += data;
+      const lines = state.content.split("\n");
+      const rows = Math.min(ROWS_MAX, lines.length);
+      if (state.term.rows !== rows) {
+        state.term.resize(COLS, rows);
+      }
     },
   };
 };
