6dc932dd1fd5c65714c0b93f54c1db8276cacd72
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-01-16 07:46:23 +0100

    Update the dollar placements in markdown export

--- src/extension/util/CommandParser.ts
+++ src/extension/util/CommandParser.ts
@@ -1,5 +1,6 @@
 import * as vscode from "vscode";
 import { Tree } from "web-tree-sitter";
+import Parser = require("web-tree-sitter");
 import { toPosition } from "./treeSitter";
 
 export default class CommandParser {
@@ -7,34 +8,46 @@ export default class CommandParser {
 
   getCommandLines(document: vscode.TextDocument) {
     return this.getTree(document)
-      .rootNode.children.filter(
-        (node) => node.type === "command" || node.type === "list"
-      )
+      .rootNode.children.filter(this.nodeIsCode)
       .map((node) => node.text);
   }
 
   getCommandTextWithPrefix(document: vscode.TextDocument) {
     let result = "";
     let lastPos = new vscode.Position(0, 0);
+    let lastCodePos = new vscode.Position(0, 0);
     const nodes = this.getTree(document).rootNode.children;
     nodes.forEach((node, i) => {
-      result += document.getText(
-        new vscode.Range(lastPos, toPosition(node.startPosition))
-      );
-      if (lastPos.line !== node.startPosition.row) {
-        result += "$ ";
+      const startPos = toPosition(node.startPosition);
+      const endPos = toPosition(node.endPosition);
+      result += document.getText(new vscode.Range(lastPos, startPos));
+      if (this.nodeIsCode(node)) {
+        if (startPos.line === 0 || startPos.line !== lastCodePos.line) {
+          result += "$ ";
+        }
+        lastCodePos = endPos;
       }
       result += node.text;
       if (i === nodes.length - 1) {
         result += document.getText(
           new vscode.Range(
-            toPosition(node.endPosition),
+            endPos,
             document.lineAt(document.lineCount - 1).range.end
           )
         );
       }
-      lastPos = toPosition(node.endPosition);
+      lastPos = endPos;
     });
     return result;
+  }
+
+  private nodeIsCode(node: Parser.SyntaxNode) {
+    switch (node.type) {
+      case "command":
+      case "declaration_command":
+      case "list":
+        return true;
+    }
+    return false;
   }
 }
